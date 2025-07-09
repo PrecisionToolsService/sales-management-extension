03f4cdecb99a889307b860e73f9e8d212a07fd2d
hmiyoshi <hmiyoshi@ptservice.com>
2025-07-09 14:01:46 -0400

    SMI-163: CSP設定の自動化

--- src/scripts/AfterInstall.php
+++ src/scripts/AfterInstall.php
@@ -1,8 +1,10 @@
 <?php
+
 use Espo\Core\Container;
 use Espo\Core\InjectableFactory;
 use Espo\Core\Utils\Config;
 use Espo\Core\Utils\Config\ConfigWriter;
+use Espo\Core\Utils\Log;
 
 class AfterInstall
 {
@@ -11,12 +13,39 @@ class AfterInstall
     public function run($container)
     {
         $this->container = $container;
+        $log = $container->getByClass(Log::class);
+        $config = $container->getByClass(Config::class);
+
+        $configWriter = $container->getByClass(InjectableFactory::class)
+            ->create(ConfigWriter::class);
+
+        $list = $config->get('clientCspScriptSourceList') ?? [];
+
+        // 追加したいドメイン
+        $requiredDomains = [
+            'https://cdn.onesignal.com',
+            'https://onesignal.com'
+        ];
+
+
+
+        // 必要なドメインがなければ追加
+        foreach ($requiredDomains as $domain) {
+            if (!in_array($domain, $list, true)) {
+                $list[] = $domain;
+            }
+        }
+
+        // 更新
+        $configWriter->set('clientCspScriptSourceList', $list);
+        $configWriter->save();
     }
 
     protected function clearCache()
     {
         try {
             $this->container->get('dataManager')->clearCache();
-        } catch (\Exception $e) {}
+        } catch (\Exception $e) {
+        }
     }
 }
